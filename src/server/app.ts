720b5861c9b557142d83eb263675d205c1b9c60f
Vladimir Veselov <vveselov@aspirity.com>
2023-10-11 13:23:00 +0700

    start

---
+++ src/server/app.ts
@@ -0,0 +1,14 @@
+import * as http from 'http';
+
+const hostName = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Let\'s do this!');
+});
+
+server.listen(port, hostName, () => {
+  console.log(`Server running at http://${hostName}:${port}`);
+});
