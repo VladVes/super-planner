0e274b2b6e8fa041d6d46d6a2b0fb73897a82e6d
Vladimir Veselov <vveselov@aspirity.com>
2023-12-02 09:33:52 +0700

    up

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,11 +4,12 @@ import * as http from 'http';
 const hostName = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const startMessage = process.env.STARTMESSAGE || 'Let\'s do this!';
+const serverMessage = process.env.SERVER_MESSAGE;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(startMessage);
+  res.end(`${startMessage}\nhost name: ${hostName}\nserver message: ${serverMessage} `);
 });
 
 server.listen(port, hostName, () => {
